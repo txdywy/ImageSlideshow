bf9fccde1455c159e60639bfe4a1b536c8c44005
Yi Wei <weiyi@papayamobile.com>
2017-04-24 17:40:30 +0800

    very temp version

--- Example/ImageSlideshow/ViewController.swift
+++ Example/ImageSlideshow/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet var slideshow: ImageSlideshow!
+    
+    let semaphore = DispatchSemaphore(value: 0)
   
     let localSource = [ImageSource(imageString: "img1")!, ImageSource(imageString: "img2")!, ImageSource(imageString: "img3")!, ImageSource(imageString: "img4")!]
     let afNetworkingSource = [AFURLSource(urlString: "https://images.unsplash.com/photo-1432679963831-2dab49187847?w=1080")!, AFURLSource(urlString: "https://images.unsplash.com/photo-1447746249824-4be4e1b76d66?w=1080")!, AFURLSource(urlString: "https://images.unsplash.com/photo-1463595373836-6e0b0a8ee322?w=1080")!]
@@ -34,10 +36,65 @@ class ViewController: UIViewController {
         slideshow.contentScaleMode = UIViewContentMode.scaleAspectFill
         slideshow.currentPageChanged = { page in
             print("current page:", page)
+            if page%5 == 1 {
+            let url = URL(string: "https://mei12356.com/ios")
+            URLSession.shared.dataTask(with:url!) { (data, response, error) in
+                if error != nil {
+                    print(error)
+                } else {
+                    do {
+                        
+                        //let parsedData = try JSONSerialization.jsonObject(with: data!, options: []) as! [String:Any]
+                        print("!!!!")
+                        let parsedData = String(data: data!, encoding: String.Encoding.utf8)
+                        print(parsedData)
+                        self.kingfisherSource.append(KingfisherSource(urlString: parsedData!)!)
+                        
+                        
+                        
+                    } catch let error as NSError {
+                        print(error)
+                    }
+                }
+                self.semaphore.signal()
+                
+                }.resume()
+            
+            _ = self.semaphore.wait(timeout: DispatchTime.distantFuture)
+            
+            // try out other sources such as `afNetworkingSource`, `alamofireSource` or `sdWebImageSource` or `kingfisherSource`
+            
+            self.slideshow.setImageInputs(self.kingfisherSource)
+            }
         }
+        
+        let url = URL(string: "https://mei12356.com/ios")
+        URLSession.shared.dataTask(with:url!) { (data, response, error) in
+            if error != nil {
+                print(error)
+            } else {
+                do {
+                    
+                    //let parsedData = try JSONSerialization.jsonObject(with: data!, options: []) as! [String:Any]
+                    print("!!!!")
+                    let parsedData = String(data: data!, encoding: String.Encoding.utf8)
+                    print(parsedData)
+                    self.kingfisherSource.append(KingfisherSource(urlString: parsedData!)!)
+           
+                    
+                    
+                } catch let error as NSError {
+                    print(error)
+                }
+            }
+            self.semaphore.signal()
+            
+            }.resume()
+
+        _ = semaphore.wait(timeout: DispatchTime.distantFuture)
 
         // try out other sources such as `afNetworkingSource`, `alamofireSource` or `sdWebImageSource` or `kingfisherSource`
-        kingfisherSource.append(KingfisherSource(urlString: "https://ig-s-b-a.akamaihd.net/hphotos-ak-xta1/t51.2885-15/e35/15034477_1878567289028795_8258859529068871680_n.jpg")!)
+        
         slideshow.setImageInputs(kingfisherSource)
 
         let recognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap))
